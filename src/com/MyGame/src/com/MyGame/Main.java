f643ffa43f3d8b3f65b32bfed9abd46f28b4cab6
Arshad Ashrafi <127494854+Aesthetics-Arshad@users.noreply.github.com>
2025-06-28 12:53:46 +0530

    Create Main.java

---
+++ src/com/MyGame/src/com/MyGame/Main.java
@@ -0,0 +1,13 @@
+
+package com.mygame;
+
+/**
+ *
+ * @author Asus
+ */
+public class Main {
+    public static void main(String []args){
+        System.out.println("Program Started");
+        MyGame game=new MyGame();
+    }
+}
